5bed09aad4feff660d961ceeeca4d70b80f0881c
E.Alamo <erosdelalamo@gmail.com>
2025-09-22 13:35:19 +0200

    Fix test location after being automatically moved by ide

--- tests/Verifactu/VerifactuSandboxTest.php
+++ tests/Verifactu/VerifactuSandboxTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace eseperio\verifactu\tests\Integration;
+namespace Verifactu;
 
 use eseperio\verifactu\models\BreakdownDetail;
 use eseperio\verifactu\models\Chaining;
